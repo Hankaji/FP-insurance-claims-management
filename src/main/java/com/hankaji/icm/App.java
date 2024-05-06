4abc6378136ff4076673ab6e717cda0e507b56c7
Hankaji <hankaji135@gmail.com>
2024-05-06 11:02:32 +0700

    Updated

--- src/main/java/com/hankaji/icm/App.java
+++ src/main/java/com/hankaji/icm/App.java
@@ -3,15 +3,31 @@ package com.hankaji.icm;
 * @author <Hoang Thai Phuc - s3978081> 
 * @version 1.0
 *
-* Libraries used: Lanterna, Gson, Apache Commons IO
+* Libraries used: JavaFX, MaterialFX
 */
+
+import com.hankaji.icm.views.LogInPage;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * The head Application program of the project
  *
  */
-public class App  {
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        Scene scene = new Scene(new LogInPage(), 640, 480);
+
+        stage.setTitle("Insurance Claim Management System");
+        stage.setScene(scene);
+        stage.show();
+    }
     public static void main( String[] args ) {
-        // TODO: JavaFX
+        launch();
     }
 }
