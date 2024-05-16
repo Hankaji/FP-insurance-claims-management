25a54afa51b8e21e0667684c2c484c9c5fe1df89
Le Thien Son <lethienson@Les-MacBook-Pro-7.local>
2024-05-16 21:32:12 +0700

    Mapped PolicyOwner

--- src/main/java/com/hankaji/icm/models/customer/PolicyOwner.java
+++ src/main/java/com/hankaji/icm/models/customer/PolicyOwner.java
@@ -1,16 +1,31 @@
 package com.hankaji.icm.models.customer;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 import com.hankaji.icm.models.Claim;
 import com.hankaji.icm.models.InsuranceCard;
+import jakarta.persistence.*;
 
-public class PolicyOwner extends Customer {
+@Entity
+@Table(name = "policy_owners")
+public class PolicyOwner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
-    protected PolicyOwner(String id, String name, InsuranceCard insuranceCard, ArrayList<Claim> claims)
-            throws IllegalArgumentException {
-        super(id, name, insuranceCard, claims);
-        
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "annual_rate")
+    private Double annualRate;
+
+    public PolicyOwner() {
     }
-    
+
+    public PolicyOwner(UUID id, String name, Double annualRate) {
+        this.id = id;
+        this.name = name;
+        this.annualRate = annualRate;
+    }
 }
