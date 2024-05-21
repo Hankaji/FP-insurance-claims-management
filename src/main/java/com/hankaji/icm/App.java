e769418c539e145010acd5841cf8dcd4daeb9d12
edward396 <s3979056@rmit.edu.vn>
2024-05-21 21:19:06 +0700

    your claim page

--- src/main/java/com/hankaji/icm/App.java
+++ src/main/java/com/hankaji/icm/App.java
@@ -1,39 +1,28 @@
 package com.hankaji.icm;
 
-
-import com.hankaji.icm.views.LogIn;
-
-/** 
-* @author <Hoang Thai Phuc - s3978081> 
-* @version 1.0
-*
-* Libraries used: JavaFX, MaterialFX
-*/
-
+import com.hankaji.icm.controllers.AdminController;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) throws Exception {
-        // Instantiate the CustomerController
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/AdminPage.fxml"));
+            Parent root = loader.load();
+            AdminController controller = loader.getController();
+            controller.setStage(primaryStage);
 
-        // Parent page = FXMLLoader.load(getClass().getResource("/fxml/RootView.fxml"));
-        LogIn logIn = new LogIn();
-
-        Scene scene = new Scene(logIn);
-        
-        // Set up the stage
-        stage.setTitle("Insurance Customer Management System");
-        stage.setScene(scene);
-        stage.setMinWidth(1600);
-        stage.setMinHeight(900);
-        stage.show();
-
-        // Check if the user is already logged in
-        logIn.getController().checkLoginStatus(scene);
+            primaryStage.setTitle("Admin Page");
+            primaryStage.setScene(new Scene(root, 1024, 580));
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
