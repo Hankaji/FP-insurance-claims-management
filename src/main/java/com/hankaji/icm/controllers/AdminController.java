cf77f613b8ac0da4f18f73187e3a560ee08b85f9
edward396 <s3979056@rmit.edu.vn>
2024-05-21 21:19:13 +0700

    your claim page

---
+++ src/main/java/com/hankaji/icm/controllers/AdminController.java
@@ -0,0 +1,47 @@
+package com.hankaji.icm.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AdminController {
+
+    private Stage stage;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    @FXML
+    private void handleAudit() {
+        navigateTo("AuditView.fxml");
+    }
+
+    @FXML
+    private void handleClaims() {
+        navigateTo("ClaimView.fxml");
+    }
+
+    @FXML
+    private void handleCustomers() {
+        navigateTo("CustomerView.fxml");
+    }
+
+    @FXML
+    private void handleDependents() {
+        navigateTo("DependentViews.fxml");
+    }
+
+    private void navigateTo(String fxmlFile) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/" + fxmlFile));
+            Parent root = loader.load();
+            stage.getScene().setRoot(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
