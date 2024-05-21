42129eeda44fac7f6928b0b704a20f43c425a34f
Le Thien Son <lethienson@Les-MBP-7.STUSGS.RMIT.EDU.VN>
2024-05-21 13:48:56 +0700

    added a new class Audit to store changes

---
+++ src/main/java/com/hankaji/icm/models/Audit.java
@@ -0,0 +1,45 @@
+package com.hankaji.icm.models;
+
+import com.hankaji.icm.models.customer.Customer;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "audits")
+public class Audit {
+    @Id
+    private UUID id;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+    @Column(name = "description")
+    private String description;
+
+    public Audit() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
